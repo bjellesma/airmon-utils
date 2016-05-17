0de3e70a8937a0029879f2fde514fae232579a35
bjellesma <william.jellesma@gmail.com>
2016-05-17 10:22:56 -0400

    Added User Input to Scripts

--- airmon-startup.sh
+++ airmon-startup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #this script will attempt to put your interface into monitor mode
+echo "Here are a list of your interfaces"
+iwconfig
+echo "Type the interface that you would like to use (Most common is wlan0)"
+read interface
+echo "Now starting aircrack-ng on interface" $interface
 airmon-ng check kill
-airmon-ng start wlan0
-airodump-ng wlan0mon
+airmon-ng start $interface
+suffix="mon"
+airodump-ng $interface$suffix
--- airmon-takedown.sh
+++ airmon-takedown.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 #this script will attempt to put your interface into normal mode
-airmon-ng stop wlan0mon
+echo "Here are a list of your interfaces"
+iwconfig
+echo "Type the interface that you would like to stop (Most commonly, monitoring interfaces have mon at the end)"
+read interface
+airmon-ng stop $interface
 service network-manager start
+echo "Your interface should be stopped. Here is your new list of interfaces to make sure"
+ifconfig
